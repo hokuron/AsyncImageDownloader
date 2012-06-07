69deafd995acf30f22b68cd025c659d54531b9da
(hokuron) <(anti.soft.b@gmail.com)>
2012-06-07 12:00:27 +0900

    Initial Commit

---
+++ AsyncImageDownloader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AsyncImageDownloader
+//
+//  Created by 清水 拓磨 on 12/06/07.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AsyncImageDownloader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AsyncImageDownloader
+//
+//  Created by 清水 拓磨 on 12/06/07.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
